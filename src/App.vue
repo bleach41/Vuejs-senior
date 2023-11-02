68a3eb596aa38b5eb132fc69bb45b24158265c71
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-11-02 18:47:25 -0400

    Falta-style-carrito

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+    import 'primevue/resources/primevue.min.css' // importa los estilos de PrimeVue
+
+</script>
+
+<template>
+    <nav class="sidebar">
+        <RouterLink :to="{ name: 'productos' }">PRODUCTOS</RouterLink>
+        <RouterLink :to="{ name: 'category' }">CATEGORIA</RouterLink>
+        <router-link :to="{ name: 'cart' }">CART</router-link>
+
+    </nav>
+
+    <RouterView />
+</template>
+
+<style scoped>
+    .sidebar {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
---
+++ src/router/Index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import CategoryListView from '../views/CategoryListView.vue'
+import ProductsListView from '../views/ProductListView.vue'
+import ShoppingCartView from '../views/ShoppingCartView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'productos',
+            component: ProductsListView
+        },
+        {
+            path: '/category',
+            name: 'category',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: CategoryListView
+        },
+        {
+            path: '/soppingcart',
+            name: 'cart',
+            component: ShoppingCartView
+        }
+    ]
+})
+
+export default router
