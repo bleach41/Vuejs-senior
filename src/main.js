b2fe69c4e0cbd9bf67c0753eacbfcf57074a978f
Tony Alejandro <ftonyalejandrofr@gmail.com>
2023-11-02 20:32:49 -0400

    arreglo?

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import PrimeVue from 'primevue/config'
+import router from './router'
 const app = createApp(App)
 
 app.use(router)
